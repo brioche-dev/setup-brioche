c71550d2a22d282b50b178dfe9a5c381012d7410
Kyle Lacy <kylewlacy@users.noreply.github.com>
2024-12-23 00:20:54 -0800

    Add AppArmor profile to fix Brioche builds on Ubuntu 24.04 (#2)
    * Update test workflow to run on multiple Ubuntu versions
    
    * Update test workflow to build an example project
    
    * Install AppArmor profile on Linux by default

--- install-brioche.sh
+++ install-brioche.sh
@@ -5,16 +5,16 @@ set -euo pipefail
 # https://github.com/brioche-dev/brioche.dev/blob/main/public/install.sh
 
 # Validate environment variables
+if [ -z "${GITHUB_PATH:-}" -o -z "${GITHUB_ACTION_PATH:-}" ]; then
+    echo '::error::$GITHUB_PATH or $GITHUB_ACTION_PATH not set! This script should be run in GitHub Actions'
+    exit 1
+fi
 if [ -z "${HOME:-}" ]; then
     echo '::error::$HOME must be set'
     exit 1
 fi
-if [ -z "${install_dir:-}" -o -z "${version:-}" ]; then
-    echo '::error::$install_dir and $version must be set'
-    exit 1
-fi
-if [ -z "${GITHUB_PATH:-}" ]; then
-    echo '::error::$GITHUB_PATH not set! This script should be run in GitHub Actions'
+if [ -z "${install_dir:-}" -o -z "${version:-}" -o -z "${install_apparmor:-}" ]; then
+    echo '::error::$install_dir, $version, and $install_apparmor must be set'
     exit 1
 fi
 
@@ -46,7 +46,7 @@ case "$install_dir" in
         ;;
 esac
 
-# Get the URL based on the OS and architecture
+# Get the OS and architecture-specific config, such as download URL and AppArmor config
 case "$OSTYPE" in
     linux*)
         case "$(uname -m)" in
@@ -57,6 +57,32 @@ case "$OSTYPE" in
                 echo "::error::Sorry, Brioche isn't currently supported on your architecture"
                 echo "  Detected architecture: $(uname -m)"
                 exit 1
+                ;;
+        esac
+
+        case "$install_apparmor" in
+            auto)
+                # Detect if we should install an AppArmor profile. AppArmor 4.0
+                # introduced new features to restrict unprivileged user
+                # namespaces, which Ubuntu 23.10 enforces by default. The
+                # Brioche AppArmor policy is meant to lift this restriction
+                # for sandboxed builds, which we only need to do on AppArmor 4+.
+                # So, we only install the policy if AppArmor is enabled and
+                # we find the config file for AppArmor abi 4.0.
+                if type aa-enabled >/dev/null && aa-enabled -q && [ -e /etc/apparmor.d/abi/4.0 ]; then
+                    should_install_apparmor=1
+                else
+                    should_install_apparmor=
+                fi
+                ;;
+            true)
+                should_install_apparmor=1
+                ;;
+            false)
+                should_install_apparmor=
+                ;;
+            *)
+                echo "::error::Invalid value for \$install_apparmor: $install_apparmor"
                 ;;
         esac
         ;;
@@ -98,3 +124,14 @@ for new_path in "${new_paths[@]}"; do
 done
 
 echo '::endgroup'
+
+
+if [ -n "$should_install_apparmor" ]; then
+    echo "::group::Installing AppArmor config"
+
+    export BRIOCHE_INSTALL_PATH="$install_dir/brioche"
+    cat "$GITHUB_ACTION_PATH/apparmor.d/brioche-gh-actions.tpl" | envsubst | sudo tee /etc/apparmor.d/brioche-gh-actions
+    sudo apparmor_parser -r /etc/apparmor.d/brioche-gh-actions
+
+    echo "::endgroup"
+fi
